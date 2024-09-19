8b213a1d33a436857b16219a82e75c765747297c
ben mccarthy <bmccarthy90@gmail.com>
2024-09-19 09:26:38 -0400

    added documentation notes

--- server/models.py
+++ server/models.py
@@ -17,10 +17,17 @@ class Customer(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # relationship to review table created. 
+    # customer.reviews gives list of reviews
+    # one customer can have multiple reviews
     reviews = db.relationship('Review', back_populates='customer')
 
+    # creates a link btw cusomer and item
+    # customer.item can be called
     items = association_proxy('reviews', 'item')
     
+    # to prevent a reccursive loop, the customer object is excluded from the Serializer
+    # this is becasue the customer object contains reviews, which contain the customer. 
     serialize_rules = ['-reviews.customer']
 
     def __repr__(self):
@@ -31,12 +38,17 @@ class Review(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     comment = db.Column(db.String)
+    # Join table, needs foreign keys because a review has 1 customer and 1 item, each with an ID
     customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
     item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
 
+    # relationship created btw Review table and Customer and Item tables
+    # review.customer or item will give the customer or item object 
+    # each review only has one item or customer
     customer = db.relationship('Customer', back_populates='reviews')
     item = db.relationship('Item', back_populates='reviews')
 
+    # exclude item and customer reviews from serializaton because each review would contain reviews, and so on
     serialize_rules = ['-customer.reviews', '-item.reviews']
 
     def __repr__(self):
@@ -46,10 +58,14 @@ class Review(db.Model, SerializerMixin):
 class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # relationship created to Review
+    # item.reviews will give the item's reviews
+    # one item can have multiple reviews
     reviews = db.relationship('Review', back_populates='item')
 
     serialize_rules = ['-reviews.item']
